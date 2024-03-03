8f1b17d1a83da59bdcabd78015dee3b070568827
Soumargha bhattacharjee <soumargha17@gmail.com>
2024-03-03 12:00:16 +0530

    commit

--- src/jsx/Coffee.jsx
+++ src/jsx/Coffee.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
-
 import consulting from '../assets/consulting.png';
 
 const Coffee = () => {
@@ -14,30 +13,34 @@ const Coffee = () => {
     setIsDrawerOpen(false);
   };
 
+  const redirectToContactPage = () => {
+    window.location.href = 'https://portfoliosoumargha.netlify.app';
+  };
+
   return (
     <div id='coffee' className="bg-gray-50 dark:bg-black border border-black rounded-lg p-8 md:p-0 mb-32 mt-10 ml-16 mr-16 text-center">
       <a href="#" className="bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-black dark:text-blue-400 mb-2">
         <img
           src={consulting}
-          className="w-32 rounded-lg mx-auto"  // Center the image using mx-auto
+          className="w-32 rounded-lg mx-auto"
           alt="Avatar"
         />
       </a>
       <h1 className="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-2">If you like my work</h1>
       <p className="mb-4 text-base font-normal text-gray-500 lg:text-lg sm:px-8 lg:px-24 dark:text-gray-400">
-        You can always buy me a cup of
+        please check out my
       </p>
       <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-6"></p>
 
       <button
         className="text-white bg-purple-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-600 focus:outline-none dark:focus:ring-purple-600"
         type="button"
-        onClick={handleDrawerToggle}
-        data-drawer-target="drawer-contact"
-        data-drawer-show="drawer-contact"
-        aria-controls="drawer-contact"
+        onClick={() => {
+          redirectToContactPage();
+          handleDrawerHide(); // Assuming you want to hide the drawer after redirecting
+        }}
       >
-        coffee
+        Portfolio
       </button>
 
       <div
@@ -48,53 +51,9 @@ const Coffee = () => {
         tabIndex="-1"
         aria-labelledby="drawer-contact-label"
       >
-        
-<h5 id="drawer-label" className="inline-flex items-center mb-6 text-base font-semibold text-gray-500 uppercase dark:text-gray-400">
-<svg className="w-4 h-4 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
-  <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z"/>
-  <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z"/>
-</svg>
-
-</h5>
-<button
-type="button"
-onClick={handleDrawerHide}
-aria-controls="drawer-contact"
-className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
->
-<svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-</svg>
-<span className="sr-only">Close menu</span>
-</button>
-<form className="mb-6">
-<div className="mb-6">
-<label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-<input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
-</div>
-<div className="mb-6">
-<label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Subject</label>
-<input type="text" id="subject" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Let us know how we can help you" required />
-</div>
-<div className="mb-6">
-<label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
-<textarea id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
-</div>
-<button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 w-full focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 block">Send message</button>
-</form>
       </div>
     </div>
   );
 };
 
 export default Coffee;
-
-
-
-
-
-
-
-
-
-
