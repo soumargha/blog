d27e8dcf63e76e85c06d12f96c78f7d07438b243
Soumargha bhattacharjee <soumargha17@gmail.com>
2024-04-08 08:04:58 +0530

    commit

--- src/jsx/Body.jsx
+++ src/jsx/Body.jsx
@@ -2,6 +2,7 @@
 import React, { useRef } from 'react';
 import soumargha2 from '../assets/soumargha2.jpg';
 import { useNavigate } from 'react-router-dom';
+import { motion } from 'framer-motion'; // Import motion from Framer Motion
 import Blogs from './Blogs';
 import Coffee from './Coffee';
 import Contact from './Contact';
@@ -18,20 +19,24 @@ const Body = () => {
   };
 
   const handleKnowMoreClick = () => {
-
     navigate('/mydescrip');
     window.scrollTo({ top: 0, behavior: 'auto' });
   };
 
   return (
-
-   
-
     <div>
-
-<Navbar/>
-      <div className="container my-24 mx-auto md:px-12 bg-cover bg-center py-8" style={{ backgroundImage: `url(https://w0.peakpx.com/wallpaper/148/727/HD-wallpaper-moon-space-black-stars.jpg)`, backgroundSize: 'cover' }}>
-        <section  className="mb-32 text-center bg-opacity-75">
+      <Navbar />
+      <motion.div
+        initial={{ x: -200, opacity: 0 }} // Initial position and opacity
+        animate={{ x: 0, opacity: 1 }} // Animation when component is mounted
+        transition={{ delay: 0.5, duration: 1.5 }} // Animation transition
+        className="container my-24 mx-auto md:px-12 bg-cover bg-center py-8"
+        style={{
+          backgroundImage: `url(https://w0.peakpx.com/wallpaper/148/727/HD-wallpaper-moon-space-black-stars.jpg)`,
+          backgroundSize: 'cover',
+        }}
+      >
+        <section className="mb-32 text-center bg-opacity-75">
           <div className="px-16 py-20 md:px-20 ">
             <h2 className="my-12 text-5xl font-bold tracking-tight  text-white">
               Hi, <br />
@@ -49,22 +54,41 @@ const Body = () => {
             </button>
           </div>
         </section>
-      </div>
+      </motion.div>
 
-      {/* changes */}
-      <section id='about' ref={bodyRef} className=" bg-black mt-10">
+      <section id="about" ref={bodyRef} className=" bg-black mt-10">
         <div className="py-8 mt-[-8rem] px-9 mx-auto max-w-screen-xl text-center lg:py-16">
-          <div className="flex flex-col lg:flex-row lg:items-center">
+          <motion.div
+            initial={{ x: -200, opacity: 0 }} // Initial position and opacity
+            animate={{ x: 0, opacity: 1 }} // Animation when component is mounted
+            transition={{ delay: 0.5, duration: 1.5 }} // Animation transition
+            className="flex flex-col lg:flex-row lg:items-center"
+          >
             <div className="lg:w-1/2 px-5">
-              <img src={soumargha2} className=" rounded-lg" alt="Soumargha Blog"  />
+              <motion.img
+                initial={{ x: -200, opacity: 0 }} // Initial position and opacity
+                animate={{ x: 0, opacity: 1 }} // Animation when component is mounted
+                transition={{ delay: 0.5, duration: 1.5 }} // Animation transition
+                src={soumargha2}
+                className=" rounded-lg"
+                alt="Soumargha Blog"
+              />
             </div>
-            <div className="lg:w-1/2 mt-8 lg:mt-0 lg:pl-12">
+            <motion.div
+              initial={{ x: -200, opacity: 0 }} // Initial position and opacity
+              animate={{ x: 0, opacity: 1 }} // Animation when component is mounted
+              transition={{ delay: 0.5, duration: 1.5 }} // Animation transition
+              className="lg:w-1/2 mt-8 lg:mt-0 lg:pl-12"
+            >
               <h1 className="mb-4 text-3xl font-extrabold tracking-tight leading-none  md:text-4xl lg:text-5xl text-white">
                 Hi, I am SOUMARGHA BHATTACHARJEE
               </h1>
               <p className="mb-4 text-base font-normal text-gray-300 lg:text-lg sm:px-8 lg:px-24 ">
-               <p>Full stack developer and a data analyst currently studying at National Institute of Technology, Agartala.
-               I love playing cricket and making some cool background music 😊. Click on know more to connect with me.</p>
+                <p>
+                  Full stack developer and a data analyst currently studying at National Institute of Technology,
+                  Agartala. I love playing cricket and making some cool background music 😊. Click on know more to
+                  connect with me.
+                </p>
               </p>
               <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
                 <button
@@ -89,17 +113,16 @@ const Body = () => {
                   </svg>
                 </button>
               </div>
-            </div>
-          </div>
+            </motion.div>
+          </motion.div>
         </div>
       </section>
-      
-      
+
       <Blogs />
       <Cards />
       <Contact />
       <Coffee />
-      <Footer/>
+      <Footer />
     </div>
   );
 };
